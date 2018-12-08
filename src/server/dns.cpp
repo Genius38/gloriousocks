796711b32e820ac117b70081015ea9fa15689641
Terrance Qin <qintairan@gmail.com>
2018-12-08 15:29:59 +0800

    Add file about dns stuff

---
+++ src/server/dns.cpp
@@ -0,0 +1,5 @@
+//
+// Created by qintairan on 18-12-8.
+//
+
+#include "dns.hpp"
---
+++ src/server/dns.hpp
@@ -0,0 +1,8 @@
+//
+// Created by qintairan on 18-12-8.
+//
+
+#ifndef GLORIOUSOCKS_DNS_HPP
+#define GLORIOUSOCKS_DNS_HPP
+
+#endif //GLORIOUSOCKS_DNS_HPP
